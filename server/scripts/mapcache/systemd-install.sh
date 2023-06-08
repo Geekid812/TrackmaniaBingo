e5c0bfca240b6b05a480317dbfdbb3482a8c085e
Geekid812 <geekid812@gmail.com>
2023-06-08 13:36:42 +0200

    mapcache: start after install

--- server/scripts/mapcache/systemd-install.sh
+++ server/scripts/mapcache/systemd-install.sh
@@ -2,5 +2,5 @@
 cat mapcache.service | sed "s+{SCRIPT_PATH}+$PWD+g" > ~/.config/systemd/user/mapcache.service
 cp -f mapcache.timer ~/.config/systemd/user/mapcache.timer
 systemctl --user daemon-reload
-systemctl --user enable mapcache.timer
-echo "mapcache service installed and enabled."
+systemctl --user start mapcache.timer
+echo "mapcache service installed and started."
