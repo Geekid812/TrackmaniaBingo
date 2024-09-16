ad4915b41a8e10b4e43ddae257b5f8a79661c2ef
Geekid812 <geekid812@gmail.com>
2024-09-16 14:57:27 +0200

    server: add LRU cache to user tokens

--- bingoserver/user.py
+++ bingoserver/user.py
@@ -1,11 +1,12 @@
 from typing import Annotated, Optional
+from functools import lru_cache
 
 from fastapi import Header, HTTPException, status
 
 from models.user import UserModel
 from models.channel import ChannelModel
 
-
+@lru_cache()
 def get_user(x_token: Annotated[str, Header()]) -> UserModel:
     if x_token == "secret":
         return UserModel(uid=0, name="Player 0", account_id="0")
